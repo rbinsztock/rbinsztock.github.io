92a23413c96409f48364ea3f2508f6f66e2ffc57
Binsztock Rémy <remy.binsztock@gmail.com>
2017-04-28 21:09:20 +0200

    modify username for default search on github

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,61 +1,43 @@
-angular.module('senayar', ['ngMaterial', 'ngAnimate'], function ($locationProvider) {
+angular.module('senayar', ['ngMaterial', 'ngAnimate'],  $locationProvider => {
   $locationProvider.hashPrefix('');
 });
 
-angular.module('senayar').controller('homeCtrl', ['$scope', 'githubService', function ($scope, githubService) {
-  $scope.username = 'senayar';
-  $scope.getGitInfo = function () {
+angular.module('senayar').controller('homeCtrl', ['$scope', 'githubService', ($scope, githubService) => {
+  $scope.username = 'rbinsztock';
+  $scope.getGitInfo = () => {
     $scope.userNotFound = false;
     $scope.reposNotFound = false;
-    githubService.checkIfUserExist($scope.username).then(function (data) {
-      if (data != null){
-        $scope.loaded = true;
-        $scope.userInfo = data;
-      }
-      else {
-        $scope.userNotFound = true;
-      }
-      githubService.getUserRepos($scope.username).then(function (data) {
-        if (data != null) {
-          $scope.repos = data;
-          console.log(data);
-          $scope.reposFound = data.length > 0;
+    githubService.checkIfUserExist($scope.username)
+      .then(data => {
+        if (data != null){
+          $scope.loaded = true;
+          $scope.userInfo = data;
         }
-      }, (function (error) {
-        $scope.reposNotFound = true;
+        else {
+          $scope.userNotFound = true;
+        }
+        githubService.getUserRepos($scope.username)
+          .then(data => {
+            if (data != null) {
+              $scope.repos = data;
+              $scope.reposFound = data.length > 0;
+            }
+          }, (error => {
+            $scope.reposNotFound = true;
+          }));
+      }, (error => {
+        $scope.userNotFound = true;
       }));
-    }, (function (error) {
-      $scope.userNotFound = true;
-    }));
   };
 
 }]);
 
-angular.module('senayar').service('twitterService', function () {
-
-});
-
-angular.module('senayar').service('githubService', ['$http', '$q', function ($http, $q) {
-  this.checkIfUserExist = function (username) {
-    var defer = $q.defer();
-    $http.get("https://api.github.com/users/" + username).success(function (data) {
-      if (data.name == "") data.name = data.login;
-      defer.resolve(data);
-    }).error(function (error) {
-      defer.reject(error);
-      return false;
-    });
-    return defer.promise;
+angular.module('senayar').service('githubService', ['$http', '$q', ($http, $q) => {
+  this.checkIfUserExist = username => {
+    return $http.get("https://api.github.com/users/" + username);
   };
 
-  this.getUserRepos = function (username) {
-    var defer = $q.defer();
-    $http.get("https://api.github.com/users/" + username + "/repos").success(function (data) {
-      defer.resolve(data);
-    }, function (error) {
-      defer.reject(error);
-      return false;
-    });
-    return defer.promise;
+  this.getUserRepos =  username => {
+    return $http.get("https://api.github.com/users/" + username + "/repos");
   };
 }]);
